976417822f5c8135fcdda00673f2d4884a257c7c
Nemexia <amirhossein.rahmani55@gmail.com>
2024-03-16 21:21:04 +0330

    add cmake

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout<<"Hello World!\n";
-    return 0;
+    int a, b;
+    std::cin>> a >> b;
+    std::cout<< a+b <<std::endl;
 }
--- python/main.py
+++ python/main.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+
+from random import randint
+# print(*iter(lambda:randint(0,99),0),0)
+
+
+random_numbers = []
+while True:
+    new_random_number = randint(0, 99)
+    random_numbers.append(new_random_number)
+    if new_random_number == 0:
+        break
+    
+
+print(random_numbers)
